84c0dd8b9ec45ba65f21a4e53c98234ace7fa65a
KH Kim <101717060+imkhkim@users.noreply.github.com>
2025-11-07 08:50:41 +0900

    add solution files

---
+++ solutions/15654.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+int N, M;
+int input[10];
+int num[10];
+int chk[10];
+
+void func(int idx) {
+    if (idx == M) {
+        for (int i = 0; i < M; ++i) {
+            cout << num[i] << ' ';
+        }    
+        cout << '\n';
+        return;
+    }
+
+    for (int i = 0; i < N; ++i) {
+        if (chk[i]) continue;
+
+        num[idx] = input[i];
+        chk[i] = true;
+        func(idx + 1);
+        chk[i] = false;
+    }
+}
+
+int main(int argc, char **argv) {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    cin >> N >> M;
+    for (int i = 0; i < N; ++i) {
+        cin >> input[i];
+    }
+
+    sort(input, input + N);
+
+    func(0);
+
+    return 0;
+}
