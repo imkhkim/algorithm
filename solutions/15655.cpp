c1944c075df72758c8f610e8615c325daa478f69
KH Kim <101717060+imkhkim@users.noreply.github.com>
2025-11-07 09:27:16 +0900

    add solution files

---
+++ solutions/15655.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+int N, M;
+int input[10];
+int num[10];
+
+void func(int idx, int cur) {
+    if (idx == M) {
+        for (int i = 0; i < M; ++i) {
+            cout << num[i] << ' ';
+        }    
+        cout << '\n';
+        return;
+    }
+
+    for (int i = cur; i < N; ++i) {
+        num[idx] = input[i];
+        func(idx + 1, i + 1);
+    }
+}
+
+int main(int argc, char **argv) {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    cin >> N >> M;
+    for (int i = 0; i < N; ++i) {
+        cin >> input[i];
+    }
+
+    sort(input, input + N);
+
+    func(0, 0);
+
+    return 0;
+}
